fc63250c6fa06690e386994a92667b23ea79203b
Lucas <lucaspistolesi07@gmail.com>
2026-05-21 16:17:50 -0300

    test

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.FRONTEND_URLS,
+    allow_origins= "*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
